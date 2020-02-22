0d37a29a7f9c3566ae214e355bcdab656e47e3de
kzk0829 <kzk0829kop@gmail.com>
2020-02-22 13:58:50 +0900

    add flask api

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,FlexSendMessage
 )
 
 app = Flask(__name__)
